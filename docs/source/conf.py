b74896fafeb8771a471a4eb310f63b51e56e5d60
DUCHEMIN Quentin <qduchemi@umr8050m-doctorant-f10.u-pem.fr>
2021-09-14 09:50:50 +0200

    updating package for B0 complex

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
     "sphinx_rtd_theme",
-    'm2r', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+    'sphinx.ext.coverage', 'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
